a331e090b3635ba37582bb284b6f8408a0b819f0
DerekKronemeyer <58587495+DerekKronemeyer@users.noreply.github.com>
2020-02-13 13:46:13 -0800

    heads grow back

--- Hydra.java
+++ Hydra.java
@@ -3,24 +3,35 @@ import java.awt.*;
 import static javax.swing.JOptionPane.showMessageDialog;
 //import javax.swing.plaf.basic.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class Hydra extends JFrame
 {
-    private JButton behead;
+    public static int age = 0;
 
     public Hydra()
     {
         super("Hydra Head");
         setSize(250, 300);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);//FIXME
+        setResizable(false);
+        Random r = new Random();
+        setLocation(r.nextInt(500), r.nextInt(500));
+        if(age<10)
+        {
+            setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        }
         JMenuBar menuBar = new JMenuBar();
             JMenu attack = new JMenu("Attack");
                 JMenuItem hit = new JMenuItem("hit");
                 hit.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){hitf();}});
                 attack.add(hit);
+                JMenuItem behead = new JMenuItem("behead");
+                behead.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){beheadf();}});
+                attack.add(behead);
             menuBar.add(attack);
             JMenu help = new JMenu("Help");
                 JMenuItem cry = new JMenuItem("Cry");
+                cry.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){cryf();}});
                 help.add(cry);
             menuBar.add(help);
         setJMenuBar(menuBar);
@@ -34,7 +45,22 @@ public class Hydra extends JFrame
 
     public void hitf()
     {
-            showMessageDialog(null, "hit did nothing");
+        showMessageDialog(null, "hit did nothing");
+    }
+
+    public void cryf()
+    {
+        showMessageDialog(null, "crying did nothing");
+    }
+
+    public void beheadf()
+    {
+        //setVisible(false);
+        dispose();
+        age = age + 1;
+        showMessageDialog(null, "Age: " + age);
+        Hydra h1 = new Hydra();
+        Hydra h2 = new Hydra();
     }
 
 }
